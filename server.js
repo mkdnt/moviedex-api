acb0b39687d0624a83133c52a08887aae14ef0be
Mike Dent <mikedent85@gmail.com>
2020-11-25 12:36:02 -0500

    commit

---
+++ server.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet')
+const cors = require('cors')
+const movies = require('./movies-data.json')
+
+const app = express();
+app.use(morgan('dev'));
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+    console.log('validate bearer token middleware')
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized Request'})
+    }
+    next();
+})
+
+function handleGetMovies(req, res){
+    const {genre, country, avg_vote} = req.query;
+    let results = [...movies];
+
+    if (country) {
+        results = results.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase())) 
+    }
+
+    if (genre) {
+        results = results.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()))
+    }
+
+    if (avg_vote) {
+        results = results.filter(movie => Number(movie.avg_vote) >= Number(avg_vote))
+    }
+
+    return res
+    .json(results)
+}
+
+app.get('/movie', handleGetMovies)
+
+app.get('/', (req, res) => {
+    let results = [...movies]
+
+    res
+    .json(results)
+})
+
+const PORT = 9000 
+app.listen(PORT, () => {console.log(`Server has started on PORT ${PORT}...`)})
